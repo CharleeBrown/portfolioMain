1a2fb6a70226e8c23469778f7d6aa8ca16600764
CharlesLee-23 <Coleman.hallom@Spray.com>
2022-05-10 10:54:03 -0500

    About Page works now.

--- routes/about.js
+++ routes/about.js
@@ -2,10 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/about', function(req, res, next) {
-  console.log("test");
+router.get('/', function(req, res, next) {
   res.render('about');
-  
 });
 
 module.exports = router;
